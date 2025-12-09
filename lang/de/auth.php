e0435d7c42bfc97c2dcb3827d4208b049c5bb8bb
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:23:46 +0100

    .

--- lang/de/auth.php
+++ lang/de/auth.php
@@ -24,11 +24,19 @@ return [
     // Anmeldung
     'login' => [
         'title' => 'Anmelden',
+<<<<<<< HEAD
+=======
+
+>>>>>>> 90b9679 (.)
         'email' => 'E-Mail',
         'password' => 'Passwort',
         'remember_me' => 'Angemeldet bleiben',
         'forgot_password' => 'Passwort vergessen?',
         'submit' => 'Anmelden',
+<<<<<<< HEAD
+=======
+
+>>>>>>> 90b9679 (.)
         'or' => 'oder',
         'create_account' => 'ein neues Konto erstellen',
         'link' => 'Anmelden',
