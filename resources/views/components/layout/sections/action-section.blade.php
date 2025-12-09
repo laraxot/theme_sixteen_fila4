801a64fe375420364c4333159645f5fc77a4eeaa
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:53:29 +0100

    .

--- resources/views/components/layout/sections/action-section.blade.php
+++ resources/views/components/layout/sections/action-section.blade.php
@@ -1,15 +1,21 @@
 <div {{ $attributes->merge(['class' => 'md:grid md:grid-cols-3 md:gap-6']) }}>
 <<<<<<< HEAD
+<<<<<<< HEAD
     <x-layout.sections.section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
     </x-layout.sections.section-title>
 =======
+=======
+>>>>>>> 0eb3291 (.)
     <x-section-title>
         <x-slot name="title">{{ $title }}</x-slot>
         <x-slot name="description">{{ $description }}</x-slot>
     </x-section-title>
+<<<<<<< HEAD
 >>>>>>> c8b07ab (.)
+=======
+>>>>>>> 0eb3291 (.)
 
     <div class="mt-5 md:mt-0 md:col-span-2">
         <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
