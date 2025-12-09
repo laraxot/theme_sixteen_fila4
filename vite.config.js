be49db65a7f4c97f6587c270924a1b8f154deac2
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:54:04 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin'
 import path from 'path';
@@ -9,12 +10,53 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+=======
+//const dotenvExpand = require('dotenv-expand');
+//dotenvExpand(
+//	require('dotenv').config({ path: '../../.env' /*, debug: true*/ }),
+//);
+
+import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
+
+
+
+export default defineConfig({
+    build: {
+        //outDir: '../../../public_html/build/ewall',
+        outDir: './public',
+        emptyOutDir: false,
+        manifest: 'manifest.json',
+        //rollupOptions: {
+        //    output: {
+        //        entryFileNames: `assets/[name].js`,
+        //        chunkFileNames: `assets/[name].js`,
+        //        assetFileNames: `assets/[name].[ext]`
+        //    }
+        //}
+    },
+    ssr: {
+        noExternal: ['chart.js/**']
+    },
+    plugins: [
+        laravel({
+            publicDirectory: '../../../public_html/',
+            // buildDirectory: 'assets/',
+            input: [
+                //__dirname + '/resources/sass/app.scss',
+                //__dirname + '/resources/scss/app-mix.scss',
+                __dirname + '/resources/css/app.css',
+                __dirname + '/resources/js/app.js',
+                // Aggiungiamo i CSS di Filament
+                //__dirname + '/vendor/filament/**/*.css',
+>>>>>>> a3dca9d (.)
             ],
             refresh: [
                 ...refreshPaths,
                 'app/Livewire/**',
             ],
         }),
+<<<<<<< HEAD
         tailwindcss(),
     ],
     build: {
@@ -24,3 +66,53 @@ export default defineConfig({
         
     },
 });
+=======
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources',
+        },
+    },
+});
+
+
+/*
+import collectModuleAssetsPaths from './../../vite-module-loader.js';
+
+async function getConfig() {
+    const paths = [
+        'resources/css/app.css',
+        'resources/js/app.js',
+    ];
+    const allPaths = await collectModuleAssetsPaths(paths, 'Modules');
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: allPaths,
+                refresh: true,
+            })
+        ]
+    });
+}
+
+export default getConfig();
+*/
+
+/*
+var $from = './resources/dist';
+var $to = '../../../public_html/themes/Five/dist';
+console.log('from :' + $from);
+console.log('to :' + $to);
+
+//mix.copyDirectory($from, $to);
+viteStaticCopy({
+    targets: [
+        {
+            src: $from,
+            dest: $to
+        }
+    ]
+})
+*/
+>>>>>>> a3dca9d (.)
