6f547fa41e7d25603a1c96761360208d4ef16f04
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:25:13 +0100

    .

--- resources/views/components/layouts/marketing.blade.php
+++ resources/views/components/layouts/marketing.blade.php
@@ -1,5 +1,24 @@
 <x-layouts.main>
+<<<<<<< HEAD
     <x-section slug="header"/>
+=======
+>>>>>>> a153bba (.)
+    {{--  
+    <x-ui.marketing.header />
+    --}}
+    {{ $_theme->headernav() }}
+
+    {{ $slot }}
+
+    {{ $_theme->footer() }}
+
+   <x-section slug="header"/>
+    {{ $slot }}
+    <x-section slug="footer"/>
+>>>>>>> origin/develop
+>>>>>>> a153bba (.)
+   <x-section slug="header"/>
+>>>>>>> a3dca9d (.)
     {{ $slot }}
     <x-section slug="footer"/>
 </x-layouts.main>
