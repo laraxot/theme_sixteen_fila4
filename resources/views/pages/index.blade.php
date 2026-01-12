70c68c951ffe5427bd226596ffa0edf54f0ae961
marco76tv <marco.sottana@gmail.com>
2026-01-12 12:29:01 +0100

    .

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -21,28 +21,16 @@ middleware($base_middleware);
 
 new class extends Component
 {
+    public string $slug='home';
 };
+
 
 ?>
 
 <x-layouts.app>
     @volt('home')
-    <div class="min-h-screen bg-gray-100">
-        <div class="grid grid-cols-1 lg:grid-cols-4 gap-6 lg:gap-8">
-            {{-- Contenuto Principale (3/4 larghezza) - Occupa 3 colonne su 4 --}}
-            <div class="lg:col-span-3">
-                <div class="bg-white rounded-lg shadow-sm p-6">
-                    <x-page side="content" slug="home" />
-                </div>
-            </div>
-
-            {{-- Sidebar Destra (1/4 larghezza) - Occupa 1 colonna su 4 --}}
-            <div class="lg:col-span-1">
-                <div class="bg-white rounded-lg shadow-sm p-6">
-                    <x-page side="sidebar" slug="home" />
-                </div>
-            </div>
-        </div>
+    <div>
+        <x-page side="content" :slug="$slug" />
     </div>
     @endvolt
 </x-layouts.app>
